cc581cf93f49486c126b66719ca87f312810bfc7
Biagiarchiefz <biagiiarchie@gmail.com>
2024-12-31 23:42:06 +0700

    push

--- sistem manajemen gudang untuk proses distribuasi barang (2).sql
+++ sistem manajemen gudang untuk proses distribuasi barang (2).sql
@@ -98,3 +98,107 @@ GO
 
 ALTER TABLE [Pengiriman] ADD FOREIGN KEY ([ID_Distribusi]) REFERENCES [Distribusi] ([ID_Distribusi])
 GO
+
+
+
+
+-- TUBES PRAKTIKUM BASIS DATA
+
+-- -- Tabel Barang
+-- CREATE TABLE Barang (
+--   ID_Barang INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   nama_barang VARCHAR(100) NOT NULL,
+--   kategori VARCHAR(100) NOT NULL,
+--   stok INT NOT NULL,
+--   stok_minimum INT NOT NULL,
+--   harga_beli DECIMAL(10, 3) NOT NULL,
+--   harga_jual DECIMAL(10, 3) NOT NULL
+-- );
+
+
+-- -- Tabel Gudang
+-- CREATE TABLE Gudang (
+--   ID_Gudang INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   nama_gudang VARCHAR(100) NOT NULL,
+--   alamat VARCHAR(200) NOT NULL,
+--   kapasitas_terisi INT,
+--   kapasitas_max INT
+-- );
+
+-- -- Tabel Rak
+-- CREATE TABLE Rak (
+--   ID_Rak INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   kode_rak NVARCHAR(255) UNIQUE,
+--   kapasitas_terisi INT,
+--   kapasitas_max INT,
+--   ID_Barang INT,
+--   ID_Gudang INT,
+--   FOREIGN KEY (ID_Barang) REFERENCES Barang(ID_Barang),
+--   FOREIGN KEY (ID_Gudang) REFERENCES Gudang(ID_Gudang)
+-- on delete cascade
+-- on update cascade
+-- );
+
+-- -- Tabel Supplier
+-- CREATE TABLE Supplier (
+--   ID_Supplier INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   nama_supplier VARCHAR(100) NOT NULL,
+--   kontak VARCHAR(10) NOT NULL,
+--   alamat VARCHAR(200) NOT NULL
+-- );
+
+
+-- -- Tabel Pelanggan
+-- CREATE TABLE Pelanggan (
+--   ID_Pelanggan INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   nama_Pelanggan VARCHAR(100) NOT NULL,
+--   kontak VARCHAR(10) NOT NULL,
+--   alamat VARCHAR(200) NOT NULL
+-- );
+
+
+-- -- Tabel Penerimaan
+-- CREATE TABLE Penerimaan (
+--   ID_Penerimaan INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   jumlah INT,
+--   tanggal_penerimaan DATETIME,
+--   ID_Barang INT,
+--   ID_Supplier INT,
+--   ID_Gudang INT,
+--   FOREIGN KEY (ID_Barang) REFERENCES Barang(ID_Barang),
+--   FOREIGN KEY (ID_Supplier) REFERENCES Supplier(ID_Supplier),
+--   FOREIGN KEY (ID_Gudang) REFERENCES Gudang(ID_Gudang)
+-- on delete cascade
+-- on update cascade
+-- );
+
+
+-- CREATE TABLE Distribusi (
+--   ID_Distribusi INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   jumlah INT,
+--   tanggal_distribusi DATETIME,
+--   status NVARCHAR(255) NOT NULL CHECK (status IN ('pending', 'berhasil', 'dibatalkan')),
+--   ID_Barang INT,
+--   ID_Gudang INT,
+--   ID_Pelanggan INT,
+--   FOREIGN KEY (ID_Barang) REFERENCES Barang(ID_Barang),
+--   FOREIGN KEY (ID_Gudang) REFERENCES Gudang(ID_Gudang),
+--   FOREIGN KEY (ID_Pelanggan) REFERENCES Pelanggan(ID_Pelanggan)
+-- on delete cascade
+-- on update cascade
+-- );
+
+
+
+-- -- Tabel Pengiriman
+-- CREATE TABLE Pengiriman (
+--   ID_Pengiriman INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+--   metode_pengiriman VARCHAR(100) NOT NULL,
+--   biaya_pengiriman DECIMAL(10, 3),
+--   tanggal_pengiriman DATETIME,
+--   status_pengiriman NVARCHAR(255) NOT NULL CHECK (status_pengiriman IN ('In Transit', 'Delivered')),
+--   ID_Distribusi INT,
+--   FOREIGN KEY (ID_Distribusi) REFERENCES Distribusi(ID_Distribusi)
+-- on delete cascade
+-- on update cascade
+-- );
